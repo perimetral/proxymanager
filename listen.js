192b667c54a8df03fef4ca5607a581f81c2abe50
perimetral <themtrx@protonmail.com>
2016-07-31 23:30:51 +0300

    proxy choice possibility

--- listen.js
+++ listen.js
@@ -3,6 +3,7 @@ global.c = require('./lib/configurator')({ initial: require('./config') });
 const Nedb = require('nedb');
 const proxy = require('http-proxy');
 const request = require('request');
+const cli = require('commander');
 
 const db = new Nedb({
 	filename: c('database filename'),
@@ -11,6 +12,21 @@ const db = new Nedb({
 db.persistence.setAutocompactionInterval(c('database autocompaction interval'));
 
 var target = '';
+
+cli.version('1.0.0').command('show').action(() => {
+	db.find({}, (e, result) => {
+		if (e) throw new Error(e);
+		if ((! result) || (! result.length)) throw new Error('No proxies!');
+		console.log(result);
+	});
+});
+cli.command('use <host:port>').action((tuple) => {
+	target = 'http://' + tuple;
+});
+cli.parse(process.argv);
+
+const util = require('util');
+util.inspect(cli);
 
 setInterval(() => {
 	db.find({}, (e, result) => {
@@ -31,8 +47,8 @@ db.find({}, (e, result) => {
 	if (e) throw new Error(e);
 	if ((! result) || (! result.length)) throw new Error('No proxies!');
 	let worker = result[0];
-	target = 'http://' + worker.host + ':' + worker.port;
-	console.log(target);
+	target = (target || ('http://' + worker.host + ':' + worker.port));
+	c('logger function')('USING ' + target + ' AS PROXY SERVER');
 	let proxyServer = proxy.createProxyServer({ target }).listen(c('listen port'), c('listen host'), () => {
 		console.log('listening on ' + c('listen port'));
 	});
